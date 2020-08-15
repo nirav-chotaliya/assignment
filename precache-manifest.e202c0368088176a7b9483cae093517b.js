194339a02531bacd97ebb72282b26b2a6478c830
Nirav Chotaliya <nirav.chotaliya@bookmyshow.com>
2020-08-16 00:13:51 +0530

    Updates

---
+++ precache-manifest.e202c0368088176a7b9483cae093517b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1a0ebe880fdd50e5d1008e09a929c2",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "ed1f744c60380ad08f4d",
+    "url": "/assignment/static/css/main.0c563f9d.chunk.css"
+  },
+  {
+    "revision": "71a9a42ec63252403cb9",
+    "url": "/assignment/static/js/2.0c7c3fa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment/static/js/2.0c7c3fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1f744c60380ad08f4d",
+    "url": "/assignment/static/js/main.c8964705.chunk.js"
+  },
+  {
+    "revision": "c93435cef9f4d61fe58a",
+    "url": "/assignment/static/js/runtime-main.8fa38930.js"
+  }
+]);
